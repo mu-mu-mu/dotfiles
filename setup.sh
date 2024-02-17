a894d3c77a96fac3347c79f4b0ca701525abdbef
Keisuke Nishimura <keisuke.nishimura@inria.fr>
2024-02-17 13:34:09 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 
+
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p  ~/.vim/rc
 ln -s -f  ~/dotfiles/dein.toml ~/.vim/rc/dein.toml
 ln -s -f  ~/dotfiles/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+
+mkdir -p ~/.tmux/plugins
 ln -s -f  ~/dotfiles/.tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 ln -s -f  ~/dotfiles/.zshrc ~/.zshrc
+touch ~/.gitconfig.local
 ln -s -f  ~/dotfiles/.gitconfig ~/.gitconfig
